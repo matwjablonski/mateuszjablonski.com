e425ae7d2a72d1f4a366e25b3a549bd90c4b4d5e
Mateusz Jabłoński <mat.w.jablonski@gmail.com>
2019-08-08 00:03:14 +0200

    recent stories headline

--- src/components/RecentStories/RecentStories.js
+++ src/components/RecentStories/RecentStories.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Grid } from "@smooth-ui/core-sc";
 
-import { RecentStoriesWrapper } from "./RecentStories.style";
+import { RecentStoriesWrapper, RecentStoriesHeadline } from "./RecentStories.style";
 
 const RecentStories = () => {
   return (
     <RecentStoriesWrapper>
-      <Grid>:recent stories</Grid>
+      <Grid>
+        <RecentStoriesHeadline>Recent stories</RecentStoriesHeadline>
+      </Grid>
     </RecentStoriesWrapper>
   );
 };
--- src/components/RecentStories/RecentStories.style.js
+++ src/components/RecentStories/RecentStories.style.js
@@ -5,8 +5,27 @@ export const RecentStoriesWrapper = styled.section`
   background: ${th("white")};
   border-top: 1px solid ${th("lighterGrey")};
   bottom: 0;
-  box-shadow: 0 0 7px 0 ${th("darkGrey")};
+  box-shadow: 0 5px 15px 0 ${th("darkGrey")};
   padding: 1em 0;
   position: fixed;
   width: 100%;
+`;
+
+
+export const RecentStoriesHeadline = styled.h3`
+  font-family: ${th('mainFont')};
+  margin: 0;
+  max-width: 100px;
+  line-height: 1;
+  font-size: 1.3em;
+
+  &::after {
+    background: ${th('primary')};
+    border-radius: 50%;
+    content: "";
+    display: inline-block;
+    height: 5px;
+    margin-left: 5px;
+    width: 5px;
+  }
 `;
