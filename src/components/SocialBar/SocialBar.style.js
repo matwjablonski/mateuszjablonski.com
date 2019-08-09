bff446153b04b3d0c5dd8c5db0b7b5ab5fdba3c7
Mateusz Jabłoński <mateusz.jablonski@sonalake.com>
2019-08-09 13:37:09 +0200

    visual change for header

--- src/components/SocialBar/SocialBar.style.js
+++ src/components/SocialBar/SocialBar.style.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 import { th } from '@smooth-ui/core-sc';
 
 export const SocialBarWrapper = styled.section`
+  border-bottom: 1px solid ${th('lighterGrey')};
   display: flex;
   justify-content: center;
-  padding: 0.5em 0 3em 0;
+  margin: 0 auto 0.5em;
+  max-width: 100%;
+  padding: 0.5em 0 4.5em 0;
+  width: 600px;
 
   a {
     color: ${th('darkGrey')};
