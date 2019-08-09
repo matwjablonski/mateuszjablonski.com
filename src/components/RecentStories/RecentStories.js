bce875d45208da39f5b1131cd7df1cb654617fd4
Mateusz Jabłoński <mateusz.jablonski@sonalake.com>
2019-08-09 10:48:14 +0200

    add recent stories to footer

--- src/components/RecentStories/RecentStories.js
+++ src/components/RecentStories/RecentStories.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { Grid } from '@smooth-ui/core-sc';
+import React, { useState, useEffect } from 'react';
+import { Grid, Row, Col } from '@smooth-ui/core-sc';
+import request from '../../helpers/request';
 
 import {
   RecentStoriesWrapper,
   RecentStoriesHeadline,
+  RecentStoriesLine,
+  RecentStoryLink,
+  RecentStoryBox,
+  RecentStory,
 } from './RecentStories.style';
 
 const RecentStories = () => {
+  const [recentStories, getRecentStories] = useState([]);
+
+  useEffect(() => {
+    request
+      .get('posts/recentStories')
+      .then(res => getRecentStories(res.data.data));
+  }, []);
+
   return (
     <RecentStoriesWrapper>
       <Grid>
-        <RecentStoriesHeadline>Recent stories</RecentStoriesHeadline>
+        <Row alignItems="center">
+          <Col xs="auto" mr="auto">
+            <RecentStoriesHeadline>Recent stories</RecentStoriesHeadline>
+          </Col>
+          <Col xs="auto">
+            <RecentStoriesLine>
+              {recentStories.length > 0
+                ? recentStories.map((story, index) => (
+                    <RecentStoryLink
+                      to={`/blog/${story.slug}`}
+                      style={{
+                        marginRight: `${-20 * index}px`,
+                        zIndex: `${recentStories.length - index}`,
+                      }}
+                    >
+                      <RecentStoryBox>
+                        <RecentStory src={story.image} />
+                      </RecentStoryBox>
+                    </RecentStoryLink>
+                  ))
+                : null}
+            </RecentStoriesLine>
+          </Col>
+        </Row>
       </Grid>
     </RecentStoriesWrapper>
   );
--- src/components/RecentStories/RecentStories.style.js
+++ src/components/RecentStories/RecentStories.style.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import { th } from '@smooth-ui/core-sc';
+import { Link } from 'react-router-dom';
 
 export const RecentStoriesWrapper = styled.section`
   background: ${th('white')};
   border-top: 1px solid ${th('lighterGrey')};
   bottom: 0;
   box-shadow: 0 -1px 27px 4px rgba(0, 0, 0, 0.07);
-  padding: 1em 0;
+  padding: 0.6em 0;
   position: fixed;
   width: 100%;
 `;
@@ -16,7 +17,7 @@ export const RecentStoriesHeadline = styled.h3`
   margin: 0;
   max-width: 100px;
   line-height: 1;
-  font-size: 1.3em;
+  font-size: 1.4em;
 
   &::after {
     background: ${th('primary')};
@@ -27,4 +28,40 @@ export const RecentStoriesHeadline = styled.h3`
     margin-left: 5px;
     width: 5px;
   }
+`;
+
+export const RecentStoriesLine = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+export const RecentStoryLink = styled(Link)`
+  display: inline-block;
+  position: relative;
+  transition: ${th('coreTiming')};
+
+  &:hover {
+    margin-right: 0 !important;
+    transition: ${th('coreTiming')};
+  }
+`;
+
+export const RecentStoryBox = styled.div`
+  background: ${th('white')};
+  border: 1px solid transparent;
+  border-radius: 50%;
+  display: flex;
+  padding: 4px;
+  transition: ${th('coreTiming')};
+
+  &:hover {
+    border-color: ${th('primary')};
+    transition: ${th('coreTiming')};
+  }
+`;
+
+export const RecentStory = styled.img`
+  border-radius: 50%;
+  height: 50px;
+  width: 50px;
 `;
