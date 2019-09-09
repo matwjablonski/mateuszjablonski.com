e0b736b882ac71d964ce60e27e9d05b91e244246
Mateusz Jabłoński <mat.w.jablonski@gmail.com>
2019-09-09 07:23:23 +0200

    add post summary

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AvatarImage, AvatarImageBox } from './Avatar.style';
 import Loading from '../Loading/Loading';
+import { MeContext } from '../../meContext';
 
-const Avatar = props => (
-  <Link to="/">
-    <AvatarImageBox>
-      {props.me ? (
-        <AvatarImage
-          src={`https://www.gravatar.com/avatar/${props.me.avatar.hash}?s=${props.size}`}
-          alt={props.me.name}
-        />
-      ) : (
-        <Loading />
-      )}
-    </AvatarImageBox>
-  </Link>
-);
+const Avatar = ({ size }) => {
+  const me = useContext(MeContext);
+
+  return (
+    <Link to="/">
+      <AvatarImageBox>
+        {me ? (
+          <AvatarImage
+            src={`https://www.gravatar.com/avatar/${me.avatar.hash}?s=${size}`}
+            alt={me.name}
+          />
+        ) : (
+          <Loading />
+        )}
+      </AvatarImageBox>
+    </Link>
+  );
+};
 
 export default Avatar;
