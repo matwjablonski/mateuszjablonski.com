0c1f50bdd82d374f40053f76cf98a0323c2ea0bb
Mateusz Jabłoński <mjablonski@woodpecker.pl>
2020-07-10 13:49:21 +0200

    add syntax highlighter for react

--- src/components/FromMarkdown/FromMarkdown.js
+++ src/components/FromMarkdown/FromMarkdown.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Parser from 'html-react-parser';
 import MarkdownIt from 'markdown-it';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const FromMarkdown = ({ text }) => {
   const md = new MarkdownIt();
 
-  // console.log('text', text);
-  // text =
-  //   "IyMgSmFrIENvbnRleHQgQVBJIG1vxbxlIG5hbSBwb23Ds2M/IAoKVyBrbGFzeWN6bmVqIGFwbGlrYWNqaSBuYXBpc2FuZWogdyBSZWFjdCBkYW5lIHPEhSBwcnpla2F6eXdhbmUgeiBnw7NyeSB3IGTDs8WCIHBvbWnEmWR6eSBrb21wb25lbnRhbWkgemEgcG9tb2PEhSBwcm9wc8Ozdy4gWmRhcnphasSFIHNpxJkgc3l0dWFjamUsIGdkeSBrb21wb25lbnQgemFnbmllxbxkxbxvbnkga2lsa2EgcG96aW9tw7N3IG5pxbxlaiBwb3RyemVidWplIGRhbnljaCB6IG5hand5xbxzemVnbyBrb21wb25lbnR1LiBQcm93YWR6aSB0byBkbyBzeXR1YWNqaSwga2llZHkgbXVzaW15IHByemVrYXphxIcgcHJvcHN5IHcgZMOzxYIgcHJ6ZXoga2lsa2Ega29tcG9uZW50w7N3Lg==";
+  const content = md.render(decodeURIComponent(escape(atob(text))));
+
   return text ? (
-    <>{Parser(md.render(decodeURIComponent(escape(atob(text)))))}</>
+    <>{Parser(content, {
+      replace: domNode => {
+        if (domNode.name === 'code' && domNode.attribs.class) {
+          const lang = domNode.attribs.class.split('-');
+          return (<SyntaxHighlighter language={lang[1]} style={darcula}>
+            {domNode.children[0].data}
+          </SyntaxHighlighter>)
+        }
+      }
+    })}</>
   ) : null;
 };
 
