5395d1dcc317090960da48358fda9d652bf28c44
Mateusz Jabłoński <mjablonski@woodpecker.pl>
2020-07-23 18:59:11 +0200

    custom code styling

---
+++ src/codeStyle.js
@@ -0,0 +1,157 @@
+export const codeStyles = {
+  "code[class*=\"language-\"]": {
+    "color": "#c5c8c6",
+    "textShadow": "0 1px rgba(0, 0, 0, 0.3)",
+    "fontFamily": "Inconsolata, Monaco, Consolas, 'Courier New', Courier, monospace",
+    "direction": "ltr",
+    "textAlign": "left",
+    "whiteSpace": "pre",
+    "wordSpacing": "normal",
+    "wordBreak": "normal",
+    "lineHeight": "1.5",
+    "MozTabSize": "2",
+    "OTabSize": "2",
+    "tabSize": "2",
+    "WebkitHyphens": "none",
+    "MozHyphens": "none",
+    "msHyphens": "none",
+    "hyphens": "none"
+  },
+  "pre[class*=\"language-\"]": {
+    "color": "#c5c8c6",
+    "textShadow": "0 1px rgba(0, 0, 0, 0.3)",
+    "fontFamily": "Inconsolata, Monaco, Consolas, 'Courier New', Courier, monospace",
+    "direction": "ltr",
+    "textAlign": "left",
+    "whiteSpace": "pre",
+    "wordSpacing": "normal",
+    "wordBreak": "normal",
+    "lineHeight": "1.5",
+    "MozTabSize": "4",
+    "OTabSize": "4",
+    "tabSize": "4",
+    "WebkitHyphens": "none",
+    "MozHyphens": "none",
+    "msHyphens": "none",
+    "hyphens": "none",
+    "padding": "1em",
+    "margin": ".5em 0",
+    "overflow": "auto",
+    "borderRadius": "0.3em",
+    "background": "#1e1e1e"
+  },
+  ":not(pre) > code[class*=\"language-\"]": {
+    "background": "#1e1e1e",
+    "padding": ".1em",
+    "borderRadius": ".3em"
+  },
+  "hljs-comment": {
+    "color": "#6a9955"
+  },
+  "hljs-meta": {
+    "color": "#569cd6"
+  },
+  "prolog": {
+    "color": "#6a9955"
+  },
+  "doctype": {
+    "color": "#6a9955"
+  },
+  "cdata": {
+    "color": "#6a9955"
+  },
+  "punctuation": {
+    "color": "#569cd6"
+  },
+  ".namespace": {
+    "Opacity": ".7"
+  },
+  "property": {
+    "color": "#ce9178"
+  },
+  "keyword": {
+    "color": "#569cd6"
+  },
+  "hljs-tag": {
+    "color": "#569cd6"
+  },
+  "hljs-class-name": {
+    "color": "#FFFFB6",
+    "textDecoration": "underline"
+  },
+  "hljs-literal": {
+    "color": "#99CC99"
+  },
+  "hljs-keyword": {
+    "color": "#99CC99"
+  },
+  "symbol": {
+    "color": "#f92672"
+  },
+  "deleted": {
+    "color": "#ce9178"
+  },
+  "number": {
+    "color": "#FF73FD"
+  },
+  "selector": {
+    "color": "#A8FF60"
+  },
+  "hljs-attr": {
+    "color": "#FFFFB6",
+    "textDecoration": "underline"
+  },
+  "string": {
+    "color": "#ce9178"
+  },
+  "char": {
+    "color": "#A8FF60"
+  },
+  "hljs-built_in": {
+    "color": "#569cd6"
+  },
+  "hljs-function": {
+    "color": "#A8FF60"
+  },
+  "hljs-params": {
+    "color": "#C6C5FE"
+  },
+  "operator": {
+    "color": "##ce9178"
+  },
+  "entity": {
+    "color": "#FFFFB6",
+    "cursor": "help"
+  },
+  "url": {
+    "color": "#96CBFE"
+  },
+  ".language-css .token.string": {
+    "color": "#99CC99"
+  },
+  ".style .token.string": {
+    "color": "#99CC99"
+  },
+  "atrule": {
+    "color": "#F9EE98"
+  },
+  "hljs-string": {
+    "color": "#F9EE98"
+  },
+  "function": {
+    "color": "#569cd6"
+  },
+  "regex": {
+    "color": "#E9C062"
+  },
+  "important": {
+    "color": "#fd971f",
+    "fontWeight": "bold"
+  },
+  "bold": {
+    "fontWeight": "bold"
+  },
+  "italic": {
+    "fontStyle": "italic"
+  }
+};
--- src/components/FromMarkdown/FromMarkdown.js
+++ src/components/FromMarkdown/FromMarkdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Parser from 'html-react-parser';
 import MarkdownIt from 'markdown-it';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { darcula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { codeStyles } from '../../codeStyle';
 
 const FromMarkdown = ({ text }) => {
   const md = new MarkdownIt();
@@ -18,7 +18,7 @@ const FromMarkdown = ({ text }) => {
             <SyntaxHighlighter
               children={domNode.children[0].data}
               language={lang[1]}
-              style={darcula}
+              style={codeStyles}
               showLineNumbers={true}
             />
           );
