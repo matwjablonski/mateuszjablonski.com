4f8ea7648efd5c039e48b96b6441f302a143de62
Mateusz Jabłoński <mjablonski@woodpecker.pl>
2020-07-24 23:03:07 +0200

    change travis conf

--- server/app.js
+++ server/app.js
@@ -5,24 +5,24 @@ const path = require('path');
 
 const absolutePath = path.resolve(__dirname + '/build');
 
-const index = express();
-index.use(cors());
-index.use(express.urlencoded({ extended: false }));
-index.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-index.use(express.static('build'));
+app.use(express.static('build'));
 
-index.get('/api', (req, res) => {
+app.get('/api', (req, res) => {
   res.statusCode = 200;
   res.json({})
 });
 
-index.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(absolutePath + '/index.html');
 });
 
 
-index.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port:', process.env.PORT);
 });
 
